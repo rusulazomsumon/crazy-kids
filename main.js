c65deb3e81607de2bb67ccd88e43125047656af7
sumon <18103020@iubat.edu>
2022-07-28 13:45:59 +0600

    change from local

---
+++ main.js
@@ -0,0 +1,34 @@
+// @@@@@@@USEFULL ARRAY MEHODS@@@@@@@@@
+//SAMPLE ARRY
+const items = [
+    {name: 'Potato', price: '30'},
+    {name: 'Onion', price: '50'},
+    {name: 'Fish', price: '330'},
+    {name: 'Capcicam', price: '80'},
+    {name: 'Oil', price: '200'},
+    {name: 'Milk', price: '80'},
+    {name: 'Coke', price: '70'},
+    {name: 'Meat', price: '630'},
+    {name: 'Salt', price: '40'},
+    {name: 'Eggs', price: '130'}
+]
+
+// $$$$$$$$FILTER METHOD filter() $$$$$$$$$$$$$$
+
+// const filteredItems = items.filter((item) => {
+//     return item.price>=100;
+// })
+// console.log(filteredItems);
+
+// $$$$$$$$MAP METHOD map() $$$$$$$$$$$$$$
+// const mappedItems = items.map((item) => {
+//     return item.name;
+// })
+// console.log(mappedItems);
+
+// $$$$$$$$find METHOD find() $$$$$$$$$$$$$$
+
+// const foundItems = items.find((item) => {
+//     return item.price === "200";
+// })
+// console.log(foundItems);
